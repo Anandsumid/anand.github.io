538d01473a3a68f5bef64422b50a28a934032660
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2021-12-08 13:08:44 +0800

    modified

--- React/scoreboard/app.js
+++ React/scoreboard/app.js
@@ -1,34 +1,75 @@
-function Header(props) {
+let players = [
+    {
+        name :'AJ',
+        score:30,
+        id:1
+    },
+    {
+        name :'Haha',
+        score:20,
+        id:2
+    },
+    {
+        name :'lolzmaa',
+        score:30,
+        id:3
+    }
+]
+class Header extends React.Component{
+    render(){
     return (<header>
-        <h1>{props.title}</h1>
-        <span className="stats">Player:{props.totalPlayers}</span>
-        </header>)
+        <h1>{this.props.title}</h1>
+        <span className="stats">Player:{this.props.totalPlayers}</span>
+    </header>)
+    }
 }
-function Player(props) {
+class Player extends React.Component{
+    render(){
     return (
         <div className='player'>
-            <span className='player-name'>{props.playername}</span>
-            <Counter score={props.score}></Counter>
+            <span className='player-name'>{this.props.playername}</span>
+            <Counter ></Counter>
         </div>
     )
+    }
 }
-function Counter(props) {
+class Counter extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            score:2
+        }
+    }
+    incrementScore(){
+        this.setState(prevState=>({
+            score: prevState.score +=1
+        }))
+    }
+    decrementScore(){
+        this.setState(prevState=>({
+            score: prevState.score-=1
+        }))
+    }
+    render(){
     return (
         <div className='counter'>
-            <button className='counter-action decrement'>-</button>
-            <span className='counter-score'>{props.score}</span>
-            <button className='counter-action increment'>+</button>
+            <button className='counter-action decrement' onClick={this.decrementScore.bind(this)}>-</button>
+            <span className='counter-score'>{this.state.score}</span>
+            <button className='counter-action increment' onClick={this.incrementScore.bind(this)}>+</button>
         </div>
     )
+    }
 }
-function Board() {
+class Board extends React.Component{
+    render(){
     return (
         <div className='scoreboard'>
-            <Header title='board' totalPlayers={3}></Header>
-            <Player playername='AJ' score={40}></Player>
+            <Header title='board' totalPlayers={this.props.initialPlayers.length}></Header>
+
+            {this.props.initialPlayers.map(player=><Player playername={player.name} score={player.score} key={player.id}></Player>)}
+
         </div>
     )
-
-
+    }
 }
-ReactDOM.render(<Board></Board>, document.getElementById('root'))
+ReactDOM.render(<Board initialPlayers = {players}></Board>, document.getElementById('root'))
